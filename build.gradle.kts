1020dac12d862d7bca619b3a48af3bdc1fd6036d
[josego16] <[josemagomez53@gmail.com]>
2025-05-11 21:15:06 +0200

    agregadas dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.plugin.serialization)
 }
 
-group = "com"
+group = "com.restapi"
 version = "0.0.1"
 
 application {
@@ -17,30 +17,46 @@ repositories {
 }
 
 dependencies {
-    implementation(libs.koin.ktor)
-    implementation(libs.koin.logger.slf4j)
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.ktor.server.content.negotiation)
-    implementation(libs.exposed.core)
-    implementation(libs.exposed.jdbc)
-    implementation(libs.h2)
-    implementation(libs.postgresql)
-    implementation(libs.ktor.server.metrics.micrometer)
-    implementation(libs.micrometer.registry.prometheus)
-    implementation(libs.ktor.server.call.logging)
-    implementation(libs.ktor.server.call.id)
-    implementation(libs.ktor.server.host.common)
-    implementation(libs.ktor.server.status.pages)
+    // ───── KTOR SERVER ─────
     implementation(libs.ktor.server.request.validation)
-    implementation(libs.ktor.server.sessions)
+    implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.auth)
     implementation(libs.ktor.server.auth.jwt)
-    implementation(libs.ktor.server.default.headers)
-    implementation(libs.ktor.server.cors)
-    implementation(libs.ktor.server.netty)
-    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.call.id)
+    implementation(libs.ktor.server.call.logging)
     implementation(libs.ktor.server.config.yaml)
-    testImplementation(libs.ktor.server.test.host)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.cors)
+    implementation(libs.ktor.server.host.common)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.status.pages)
+    implementation(libs.ktor.server.default.headers)
+    implementation(libs.ktor.server.sessions)
+
+    // ───── DATABASE ─────
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.java.time)
+    implementation(libs.h2)
+    implementation(libs.postgresql)
+
+    // ───── LOGGING ─────
+    implementation(libs.logback.classic)
+
+    // ───── MONITORING ─────
+    implementation(libs.micrometer.registry.prometheus)
+    implementation(libs.ktor.server.metrics.micrometer)
+
+    // ───── SECURITY ─────
+    implementation(libs.bcrypt)
+
+    // ───── OTHER ─────
+    implementation(libs.koin.ktor)
+    implementation(libs.koin.logger.slf4j)
+
+    // ───── TESTING ─────
     testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.ktor.server.test.host)
 }
