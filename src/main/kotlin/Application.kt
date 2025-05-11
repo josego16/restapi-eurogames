50d331e907e06af33bdcd27ab4cefb4497fcec9d
[josego16] <[josemagomez53@gmail.com]>
2025-05-11 20:50:00 +0200

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,17 @@
+package com
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureFrameworks()
+    configureSerialization()
+    configureDatabases()
+    configureMonitoring()
+    configureSecurity()
+    configureHTTP()
+    configureRouting()
+}
---
+++ src/main/kotlin/Serialization.kt
@@ -0,0 +1,35 @@
+package com
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.http.content.*
+import io.ktor.server.metrics.micrometer.*
+import io.ktor.server.plugins.callid.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.plugins.requestvalidation.RequestValidation
+import io.ktor.server.plugins.requestvalidation.ValidationResult
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import io.micrometer.prometheus.*
+import java.sql.Connection
+import java.sql.DriverManager
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.*
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
+import org.slf4j.event.*
+
+fun Application.configureSerialization() {
+}
