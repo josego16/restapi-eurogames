a028b6d3e4a7b11f0c110f28caaebeedfd28e63f
[josego16] <[josemagomez53@gmail.com]>
2025-05-14 12:53:18 +0200

    ruta para metricas

--- src/main/kotlin/ktor/plugins/Routing.kt
+++ src/main/kotlin/ktor/plugins/Routing.kt
@@ -1,11 +1,17 @@
 package ktor.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import ktor.prometheusRegistry
 import ktor.routes.*
 
 fun Application.configureRouting() {
     routing {
+        get("/metrics"){
+            call.respondText(prometheusRegistry.scrape())
+        }
+
         userRouting()
         countryRouting()
         gameRouting()
