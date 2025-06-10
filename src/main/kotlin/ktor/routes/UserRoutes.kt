d0b06976e832e090ce24be61679f99d79e8d6672
josego16 <josemagomez.devs@gmail.com>
2025-06-10 18:46:05 +0200

    agregado soporte de autenticación JWT en las rutas de usuario

--- src/main/kotlin/ktor/routes/UserRoutes.kt
+++ src/main/kotlin/ktor/routes/UserRoutes.kt
@@ -5,43 +5,46 @@ import domain.dto.UserRegisterDto
 import domain.dto.UserUpdateDto
 import domain.usecase.user.ProviderUserUseCase
 import io.ktor.http.*
+import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Routing.userRouting() {
-    route("/users") {
-        get {
-            val users = ProviderUserUseCase.getAllUsers()
-            call.respond(users)
-        }
-        get("/{id}") {
-            val idParam = call.parameters["id"]
-            val id = idParam?.toIntOrNull()
-            if (id == null) {
-                call.respond(HttpStatusCode.BadRequest, "Id no valido")
-                return@get
+    authenticate("jwt-auth") {
+        route("/users") {
+            get {
+                val users = ProviderUserUseCase.getAllUsers()
+                call.respond(users)
             }
-            val user = ProviderUserUseCase.getUserById(id)
-            if (user == null) {
-                call.respond(HttpStatusCode.NotFound, "Usuario no encontrado")
-            } else {
-                call.respond(user)
+            get("/{id}") {
+                val idParam = call.parameters["id"]
+                val id = idParam?.toIntOrNull()
+                if (id == null) {
+                    call.respond(HttpStatusCode.BadRequest, "Id no valido")
+                    return@get
+                }
+                val user = ProviderUserUseCase.getUserById(id)
+                if (user == null) {
+                    call.respond(HttpStatusCode.NotFound, "Usuario no encontrado")
+                } else {
+                    call.respond(user)
+                }
             }
-        }
-        patch("/{id}") {
-            val updateUser = call.receive<UserUpdateDto>()
-            val idParam = call.parameters["id"]
-            val idUser = idParam?.toIntOrNull()
-            if (idUser == null) {
-                call.respond(HttpStatusCode.BadRequest, "Id no valido")
-                return@patch
-            }
-            val user = ProviderUserUseCase.updateUser(idUser, updateUser)
-            if (user == null) {
-                call.respond(HttpStatusCode.NotFound, "No se ha podido actualizar el usuario")
-            } else {
-                call.respond(user)
+            patch("/{id}") {
+                val updateUser = call.receive<UserUpdateDto>()
+                val idParam = call.parameters["id"]
+                val idUser = idParam?.toIntOrNull()
+                if (idUser == null) {
+                    call.respond(HttpStatusCode.BadRequest, "Id no valido")
+                    return@patch
+                }
+                val user = ProviderUserUseCase.updateUser(idUser, updateUser)
+                if (user == null) {
+                    call.respond(HttpStatusCode.NotFound, "No se ha podido actualizar el usuario")
+                } else {
+                    call.respond(user)
+                }
             }
         }
     }
